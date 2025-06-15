e715cbcc206ebdbb3184e74fe82380a55b7c5718
WalidJer <walid.jerjawi@keyin.com>
2025-06-15 17:21:10 -0230

    Add initial entity classes: City, Airport, Aircraft, Passenger

--- src/main/java/rest/Aircraft/Aircraft.java
+++ src/main/java/rest/Aircraft/Aircraft.java
@@ -1,4 +1,90 @@
 package rest.Aircraft;
 
+import jakarta.persistence.*;
+import rest.Airport.Airport;
+import rest.Passenger.Passenger;
+
+import java.util.List;
+
+@Entity
 public class Aircraft {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+    private String airlineName;
+    private int capacity;
+
+
+    @ManyToMany(mappedBy = "aircraftList")
+    private List<Passenger> passengers;
+
+    @ManyToMany
+    @JoinTable(
+            name = "aircraft_airport",
+            joinColumns = @JoinColumn(name = "aircraft_id"),
+            inverseJoinColumns = @JoinColumn(name = "airport_id")
+    )
+
+    private List<Airport> airports;
+
+
+    public Aircraft() {
+    }
+
+    public Aircraft(String model, String airlineName, int capacity) {
+        this.model = model;
+        this.airlineName = airlineName;
+        this.capacity = capacity;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
+
+
 }
--- src/main/java/rest/Airport/Airport.java
+++ src/main/java/rest/Airport/Airport.java
@@ -1,4 +1,68 @@
 package rest.Airport;
 
+import jakarta.persistence.*;
+import rest.Aircraft.Aircraft;
+import rest.City.City;
+
+import java.util.List;
+
+@Entity
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToMany(mappedBy = "airports")
+    private List<Aircraft> aircraftList;
+
+    public Airport() {
+    }
+
+    public Airport(String name, String code, City city) {
+        this.name = name;
+        this.code = code;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+
+    public List<Aircraft> getAircraftList() {
+        return aircraftList;
+    }
+    public void setAircraftList(List<Aircraft> aircraftList) {
+        this.aircraftList = aircraftList;
+    }
 }
